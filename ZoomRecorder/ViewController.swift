6568fecf8a6b5d5fad3402822422593fcc298040
Patrick Aubin <paubin12@gmail.com>
2017-07-31 18:28:29 -0700

    Initial Commit

---
+++ ZoomRecorder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZoomRecorder
+//
+//  Created by Patrick Aubin on 7/31/17.
+//  Copyright © 2017 Patrick Aubin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
