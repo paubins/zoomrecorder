deb3f2394f3040c4c676c95e88998ff6abd474cf
Patrick Aubin <paubin12@gmail.com>
2017-08-03 11:04:36 -0700

    first commit

---
+++ ZoomRecorder/ZoomRecorder-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+#import "ExporterController.h"
+#import "JKScreenRecorder.h"
+#import "IosAudioController.h"
+#import "Doorbell/Doorbell.h"
+#import "FeedbackController.h"
+#import "AVAsset+VideoOrientation.h"
